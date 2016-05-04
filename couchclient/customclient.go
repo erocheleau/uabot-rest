d86ac9f00a5fff378435841de5071500ccfa24be
erocheleau <erocheleau@coveo.com>
2016-05-04 17:12:53 -0400

    couchclient custom client

--- couchclient/customclient.go
+++ couchclient/customclient.go
@@ -5,49 +5,90 @@ import (
 	"fmt"
 	"io/ioutil"
 	"net/http"
-	"strconv"
 )
 
-type Client interface {
-	GetUuid() (string, error)
-}
-
 type CouchConfig struct {
-	url  string
-	port int
+	Server string
 }
 
-type client struct {
+type CouchClient interface{}
+
+type couchClient struct {
 	httpClient *http.Client
-	url        string
-	port       int
+	server     string
 }
 
-func NewClient(c CouchConfig) Client {
-	return &client{
-		url:        c.url,
-		port:       c.port,
+type ViewResult struct {
+	ID    string      `json:"id"`
+	Key   interface{} `json:"key"`
+	Value interface{} `json:"value"`
+}
+
+type ViewResponse struct {
+	TotalRows int          `json:"total_rows"`
+	Offset    int          `json:"offset"`
+	Rows      []ViewResult `json:"rows,omitempty"`
+}
+
+func NewCouchClient(c CouchConfig) CouchClient {
+	return &couchClient{
 		httpClient: http.DefaultClient,
+		server:     c.Server,
 	}
 }
 
-type uuidResponse struct {
-	Uuids []string `json:"uuids"`
-}
-
-func (c *client) GetUuid() (string, error) {
-	var uuids uuidResponse
-
-	resp, err := http.Get(c.url + ":" + strconv.Itoa(c.port) + "/_uuids")
+func (c *couchClient) GetAllDocs(database string) (results ViewResponse, err error) {
+	resp, err := http.Get(c.server + "/" + database + "/_all_docs")
 	if err != nil {
-		return "", err
+		panic(err)
 	}
 	defer resp.Body.Close()
 	body, err := ioutil.ReadAll(resp.Body)
 
-	if err := json.Unmarshal(body, &uuids); err != nil {
-		return "", fmt.Errorf("Error parsin json response: %v\n", err)
+	if err := json.Unmarshal(body, &results); err != nil {
+		return ViewResponse{}, fmt.Errorf("Error parsin json response: %v\n", err)
 	}
 
-	return uuids.Uuids[0], nil
+	return results, nil
+}
+
+func (c *couchClient) GetView(database string, designdoc string, view string, params ...string) (results ViewResponse, err error) {
+
+	fullpath := buildURL(c.server, database, "_design", designdoc, "_view", view)
+
+	if params != nil {
+		fullpath = buildURLParams(fullpath, params)
+	}
+
+	resp, err := http.Get(fullpath)
+	if err != nil {
+		panic(err)
+	}
+	defer resp.Body.Close()
+	body, err := ioutil.ReadAll(resp.Body)
+
+	if err := json.Unmarshal(body, &results); err != nil {
+		return ViewResponse{}, fmt.Errorf("Error parsin json response: %v\n", err)
+	}
+
+	return results, nil
+}
+
+func buildURL(base string, segments ...string) string {
+	result := base
+	for _, seg := range segments {
+		result = result + "/" + seg
+	}
+	return result
+}
+
+func buildURLParams(urlBase string, params []string) string {
+	strParams := ""
+	for i, param := range params {
+		if i > 0 {
+			strParams = strParams + "&"
+		}
+		strParams = strParams + param
+	}
+	return urlBase + "?" + strParams
 }
